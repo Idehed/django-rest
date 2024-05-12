ff7807ce1841fe329d1668be4cea115a1ea12bae
Idehed anton <anton.idehed@telia.com>
2024-05-12 18:12:39 +0000

    started project, created profiles app

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -11,7 +11,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
+if os.path.exists('env.py'):
+    import env
+
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+}
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +34,7 @@ SECRET_KEY = 'django-insecure-0z-994!sqeb7nbbe%q4@cau@+kituv)fd_6whda0zz5=k@ou!)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-idehed-djangorest-42hy235x8o2.ws-eu111.gitpod.io']
 
 
 # Application definition
@@ -36,7 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
+
+    'profiles',
 ]
 
 MIDDLEWARE = [
