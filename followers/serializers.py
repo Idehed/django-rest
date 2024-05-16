2b9daa2c8dae0ddab7ccb7e1459a3a0274074a3b
Idehed anton <anton.idehed@telia.com>
2024-05-16 18:04:59 +0000

    created followers

---
+++ followers/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Follower
+from django.db import IntegrityError
+
+class FollowerSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Follower
+        fields = [
+            'id', 'owner', 'followed','followed_name', 'created_at',
+        ]
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
